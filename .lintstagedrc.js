a94331c75b05e373aa4132094ad09243a557fe27
Tim Carry <tim@pixelastic.com>
2021-07-08 23:57:36 +0200

    Update lintstaged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,11 @@
 const config = require('aberlaas/lib/configs/lintstaged.js');
+
+// Stop linting all the json files
+const jsonLintCommand = config['*.json'];
+delete config['*.json'];
+
 module.exports = {
   ...config,
+  './*.json': jsonLintCommand,
 };
+
