30227afd225ae51fcd15e1d2970272980307cff4
undefined <undefined>
2021-09-04 17:04:30 +0200

    update dailyUpdate

--- scripts/ci-dailyUpdate.js
+++ scripts/ci-dailyUpdate.js
@@ -5,31 +5,29 @@ const consoleInfo = require('firost/consoleInfo');
 const _ = require('golgoth/lodash');
 
 (async () => {
-  await onCircle.run(
-    async ({ success, gitChangedFiles, gitCommitAll, gitPush }) => {
-      consoleInfo('Running incremental update');
-      await run('yarn run data:incremental');
+  await onCircle.run(async ({ success, repo }) => {
+    consoleInfo('Running incremental update');
+    await run('yarn run data:incremental');
 
-      const changedFiles = await gitChangedFiles();
-      if (_.isEmpty(changedFiles)) {
-        success('No files changed, we stop early');
-      }
-
-      // Commit changes
-      const currentDate = dayjs.utc().format('YYYY-MM-DD');
-      const commitMessage = `chore(ci): Daily update (${currentDate})`;
-      consoleInfo(`${changedFiles.length} files changed, commiting`);
-      await gitCommitAll(commitMessage);
-
-      // And push
-      consoleInfo('Pushing changes to the repo');
-      await gitPush();
-
-      // Re-index into Algolia
-      consoleInfo('Updating Algolia index');
-      await run('yarn run data:indexing');
-
-      success('Daily data updated');
+    const status = await repo.status();
+    if (_.isEmpty(status)) {
+      success('No files changed, we stop early');
     }
-  );
+
+    // Commit changes
+    const currentDate = dayjs.utc().format('YYYY-MM-DD');
+    const commitMessage = `chore(ci): Daily update (${currentDate})`;
+    consoleInfo(`${status.length} files changed, commiting`);
+    await repo.commitAll(commitMessage);
+
+    // And push
+    consoleInfo('Pushing changes to the repo');
+    await repo.push();
+
+    // Re-index into Algolia
+    consoleInfo('Updating Algolia index');
+    await run('yarn run data:indexing');
+
+    success('Daily data updated');
+  });
 })();
